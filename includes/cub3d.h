bdef48c7457d13fbb476ffe150930c276bac1141
Pedro Melo <pedromelocf@yahoo.com>
2024-09-25 13:52:12 -0300

    fixing norm

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,6 +1,18 @@
-#ifndef CUB3D_H
-#define CUB3D_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmelo-ca <pmelo-ca@student.42.fr  >        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 22:54:02 by pmelo-ca          #+#    #+#             */
+/*   Updated: 2024/09/25 11:35:27 by pmelo-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "../libraries/lib/libft.h"
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libraries/lib/libft.h"
 
 #endif
