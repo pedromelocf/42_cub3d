516ad4ba17b21b94ba5669462bfc0c34b5a33583
mendes-jv <mendes.silva.jv@gmail.com>
2024-10-28 21:20:10 -0300

    feat(tests): add initial map validation tests
    Introduced new file for map validation tests with placeholders. This addition sets up initial structure for future test case expansions.

---
+++ tests/map_validation.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_validation.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 20:43:00 by jovicto2          #+#    #+#             */
+/*   Updated: 2024/10/28 20:43:12 by jovicto2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <criterion/criterion.h>
+#include "../includes/cub3d.h"
+//TODO: Include for tester
+
+Test(map_validation, test1) //TODO: Change test name
+{
+	cr_assert(1, "Vai corinthians"); //TODO: Change function name
+}
