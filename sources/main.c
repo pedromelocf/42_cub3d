7f1bfd1e2f7c780985a46f2785f227fca19bbcf8
mendes-jv <mendes.silva.jv@gmail.com>
2024-11-14 20:36:00 -0300

    Add end-to-end execution test with Valgrind
    Created a new test file `test_execution.c` to perform end-to-end execution tests using Valgrind. Updated `Makefile` to include test targets and added necessary headers and system calls in `cub3d.h` and `main.c` to support the test infrastructure.

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 22:54:02 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/09/25 12:49:29 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/11/13 23:08:10 by jovicto2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,19 @@ int	main(int argc, char **argv)
 {
 //	t_scene	scene;
 	(void)argv;
-	if (argc != 2)
-		handle_error(MSG_ARG_COUNT);
+//	char *i = malloc(33);
+//	ft_memset(i, 0, 33);
+//	i[0] = '0';
+//	printf(still reachable main: %c\n", *i);
+//	ssize_t f = open("test.txt", O_RDONLY);
+//	pid_t pido = fork();
+//	if (pido != 0)
+//		execl("/bin/ls", "ls", "42", NULL);
+//	else {
+		if (argc != 2)
+			handle_error(MSG_ARG_COUNT);
+//		wait(NULL);
+//	}
 //	load_scene(&scene, ++argv);
 	return (EXIT_SUCCESS);
 }
